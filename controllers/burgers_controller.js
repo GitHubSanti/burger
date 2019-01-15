d74b0c127ff837e4c8547337cfc2a30ab3bd0717
Spiderman <santiagosepulveda@me.com>
2019-01-15 11:23:30 -0500

    handlebars modifications

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,15 +7,28 @@ var router = express.Router();
 // Import the model (burgers.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+// Straight from ORM
+var orm = require("../config/orm.js");
+
 router.get("/", function(req, res) {
     burger.all(function(data) {
-      var hbsObject = {
-        allBurgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+        var hbsObject = {
+            allBurgers: data
+        };        
+        console.log("From model.js file");
+        console.log(data);
+        // res.render("index", hbsObject);
     });
-  });
+
+    orm.selectAll("burgers", function(data) {
+        console.log("from orm.js file");
+        console.log(data);
+        var hbsObject = {
+            allBurgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
 
 // Export routes for server.js to use.
-module.exports = router;  
+module.exports = router;
