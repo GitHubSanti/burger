3f2aa0aa1be5c95cbddee870c780828b8d844e28
Spiderman <santiagosepulveda@me.com>
2019-02-10 14:49:34 -0500

    modifications for JAWSDB addon

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // Used to mask MySQL Creds
 let db =  require("./MySQLCredentials.js")
-
+require("dotenv").config();
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: db.username,
-  password: db.password,
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: db.username,
+    password: db.password,
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
